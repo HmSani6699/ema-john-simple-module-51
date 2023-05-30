9126e6eda616b63c9f04c5109dcb76630d1a9823
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-05-30 20:38:56 +0600

    load to pagition data

--- src/Loader/loader.js
+++ src/Loader/loader.js
@@ -1,10 +1,23 @@
 import { getShoppingCart } from "../utilities/fakedb";
 
 const loadedCartProduct = async () => {
-    const loadProduct = await fetch('http://localhost:5000/products');
-    const products = await loadProduct.json();
 
     const storedCart = getShoppingCart();
+    const ids = Object.keys(storedCart)
+    console.log('all ids', ids);
+
+    const loadProduct = await fetch('http://localhost:5000/productsIDS', {
+        method: "POST",
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(ids)
+    });
+
+
+    const products = await loadProduct.json();
+    console.log(products);
+
     const storedProduct = [];
     for (const id in storedCart) {
         const getProduct = products.find(product => product._id === id);
